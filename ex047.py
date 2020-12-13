e67bd06d3aec61bed4b2f6d78565f8e40973a651
MarVinCosMo <74730632+marvincosmo@users.noreply.github.com>
2020-12-13 00:46:00 -0300

    Inclusão de exercícios

---
+++ ex047.py
@@ -0,0 +1,11 @@
+""" 47 - Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50. """
+'''
+for c in range (1, 51):
+    if c % 2 == 0:
+        print(c, end=' ')
+print('Pronto!')
+'''
+# Resposta do professor:
+for n in range(2, 51, 2):
+    print(n, end=' ')
+print('Pronto!')
